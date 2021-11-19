855d0f4fa3e331b6de3d2dd91fdeff5fcf852fa1
v8phan <56612604+v8phan@users.noreply.github.com>
2021-11-18 20:40:46 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+app.post('/', function() {console.log('this is a POST');});
+app.get('/', function() {
+    console.log('this is a GET');
+    res.writeHead(200);res.end();
+});
