02a4c128963608dd2d11035ca0d067e971564bfb
v8phan <56612604+v8phan@users.noreply.github.com>
2022-03-07 20:11:41 -0600

    displaycomments

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
+//const path = require('path');
 const port = process.env.PORT || 3001;
-const { nextTick } = require('process');
-const pino = require('express-pino-logger')();
+//const { nextTick } = require('process');
+//const pino = require('express-pino-logger')();
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(pino);
+//app.use(pino);
 
 const db = require('./config/database');
 const User = require("./models/User");
 const Comment = require("./models/Comment");
 
 // test database
-db.authenticate()
-  .then(() => console.log('Database connected...'))
-  .catch((err) => console.log('Error: ', err));
+// db.authenticate()
+//   .then(() => console.log('Database connected...'))
+//   .catch((err) => console.log('Error: ', err));
 
-console.log(db);
+// console.log(db);
 
-db.sync();
-console.log('The table for the model was just (re)created!');
+// db.sync();
+// console.log('The table for the model was just (re)created!');
 
 app.use(express.json());
 
